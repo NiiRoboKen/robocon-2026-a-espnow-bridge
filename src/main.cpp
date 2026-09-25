51782e2877d3700e06a0a9e7a48cd5069726829e
RRiverWest <147599678+RRiverWest@users.noreply.github.com>
2026-09-25 21:05:00 +0900

    fix: 座標の構造体の整数型データサイズを追記

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #include <Arduino.h>
 #include <ArduinoJson.h>
+#include <cstdint>
 
 struct PositionData {
-  int x = 0;
-  int y = 0;
-  int direction = 0;
+  int16_t x = 0;
+  int16_t y = 0;
+  int16_t direction = 0;
 };
 
 static PositionData targetPosition;
